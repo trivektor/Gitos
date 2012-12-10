71a9cb4b9d2b16051ce1567a7ef59f938835e507
trivektor <trivektor@gmail.com>
2012-12-09 17:36:37 -0800

    Initial Commit

---
+++ Gitos/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Gitos
+//
+//  Created by Tri Vuong on 12/9/12.
+//  Copyright (c) 2012 Crafted By Tri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Gitos/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Gitos
+//
+//  Created by Tri Vuong on 12/9/12.
+//  Copyright (c) 2012 Crafted By Tri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
