87d3d9d0716191110750a7ee914e2114fc0cb2e8
trivektor <trivektor@gmail.com>
2012-12-10 21:17:37 -0800

    styling login view controller

--- Gitos/Controllers/LoginViewController.h
+++ Gitos/Controllers/LoginViewController.h
@@ -9,11 +9,18 @@
 #import <UIKit/UIKit.h>
 #import <QuartzCore/QuartzCore.h>
 
-@interface LoginViewController : UIViewController
+@interface LoginViewController : UIViewController <UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate>
 {
-    __weak IBOutlet UITextField *emailField;
-    __weak IBOutlet UITextField *passwordField;
+    __weak IBOutlet UITableView *loginTable;
+    
+    __weak IBOutlet UITextField *emailTextField;
+    __weak IBOutlet UITextField *passwordTextField;
 }
+
+@property (nonatomic, retain) IBOutlet UITableViewCell *emailCell;
+@property (nonatomic, retain) IBOutlet UITableViewCell *passwordCell;
+
+
 
 - (void)login;
 
--- Gitos/Controllers/LoginViewController.m
+++ Gitos/Controllers/LoginViewController.m
@@ -14,6 +14,8 @@
 
 @implementation LoginViewController
 
+@synthesize emailCell, passwordCell;
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -28,22 +30,19 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     [self.navigationItem setTitle:@"Login"];
-    [self.navigationController.navigationBar setTintColor:[UIColor grayColor]];
+    [self.navigationController.navigationBar setTintColor:[UIColor colorWithRed:49/255.0 green:49/255.0 blue:49/255.0 alpha:1.0]];
     
     UIBarButtonItem *loginButton = [[UIBarButtonItem alloc] initWithTitle:@"Login" style:UIBarButtonItemStyleBordered target:self action:@selector(login)];
     [loginButton setTintColor:[UIColor colorWithRed:0 green:127/255.0 blue:245/255.0 alpha:1.0]];
     
     [self.navigationItem setRightBarButtonItem:loginButton];
     
-    [emailField.layer setBorderColor:[UIColor colorWithRed:175/255.0 green:175/255.0 blue:175/255.0 alpha:1.0].CGColor];
-    [emailField.layer setCornerRadius:4.0f];
-    [emailField.layer setMasksToBounds:YES];
-    [emailField.layer setBorderWidth:1.0f];
+    [loginTable setBackgroundColor:[UIColor clearColor]];
+    [loginTable setOpaque:NO];
+    [loginTable setSeparatorColor:[UIColor colorWithRed:198/255.0 green:198/255.0 blue:198/255.0 alpha:1.0]];
+    [loginTable setBackgroundView:nil];
     
-    [passwordField.layer setBorderColor:[UIColor colorWithRed:175/255.0 green:175/255.0 blue:175/255.0 alpha:1.0].CGColor];
-    [passwordField.layer setCornerRadius:4.0f];
-    [passwordField.layer setMasksToBounds:YES];
-    [passwordField.layer setBorderWidth:1.0f];
+    self.view.backgroundColor = [UIColor colorWithRed:223/255.0 green:223/255.0 blue:223/255.0 alpha:1.0];
 }
 
 - (void)didReceiveMemoryWarning
@@ -55,6 +54,40 @@
 - (void)login
 {
     
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 2;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row == 0) {
+        return emailCell;
+    }
+
+    if (indexPath.row == 1) {
+        return  passwordCell;
+    }
+    
+    return nil;
+}
+
+- (BOOL)textFieldShouldEndEditing:(UITextField *)textField
+{
+    return YES;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
 }
 
 @end
