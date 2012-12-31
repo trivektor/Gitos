f07c3e34d033b98e1bb76d24f33e9fe495689c04
trivektor <trivektor@gmail.com>
2012-12-30 21:32:19 -0800

    add empty RepoViewController

---
+++ Gitos/Controllers/RepoViewController.h
@@ -0,0 +1,13 @@
+//
+//  RepoViewController.h
+//  Gitos
+//
+//  Created by Tri Vuong on 12/30/12.
+//  Copyright (c) 2012 Crafted By Tri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RepoViewController : UIViewController
+
+@end
